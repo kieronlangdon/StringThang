de8d6f3c829409e74eae34825fc281492746c1e5
kieronlangdon <kieronlangdon@gmail.com>
2015-04-03 15:03:05 +0100

    Added manager with ratio

--- MixString.py
+++ MixString.py
@@ -2,10 +2,10 @@ import sys,string,random,getopt
 
 
 def show_help():
-	print './MixString.py <AmountOfNamesRequired> <emailAddress> <password> <OrgStartNumber>'
+	print './MixString.py <AmountOfNamesRequired> <emailAddress> <password> <OrgStartNumber> <personManager>'
 	print './MixString.py 2 @something.com passw0rd 6000'
-	print 'Outputs: Num:0 MickFLannery,MickFLannery@something.com,passw0rd,6000\n Num:1 JoeJose,JoeJose@something.com,passw0rd,6000'
-	print 'Outputs: int,"text","text","text","text","int"'
+	print 'Outputs: Num:1 MickFlannery,MickFlannery@something.com,Student,passw0rd,6000,Root\n Num:2 JoeJose,JoeJose@something.com,Student,passw0rd,6000,MickFlannery'
+	print 'Outputs: int,"text","text","text","text", int,"text"'
 	
 
 	
@@ -25,10 +25,10 @@ def main(argv):
 		sys.exit(0)
 	emailAddress = str(sys.argv[2])
 	password = str(sys.argv[3])
-	OrgStartNumber = int(sys.argv[5])
+	OrgStartNumber = int(sys.argv[4])
 	
 	#with open("TestMembers.txt") as f:
-	with open("yob1998.txt") as f:
+	with open("yob1880.txt") as f:
 		FileToRead = f.read().split('\n')
 
 	rows = int(sys.argv[1])
@@ -36,9 +36,10 @@ def main(argv):
 	for num in range(0,rows):
 		
 		#Some Ratio Things
-		#Check if every nth value is equal to 25 Assign role
+		#Check if every nth value is equal to 25 Assign position + Manager
 		if (int(num+1) % 25 == 0 or int(num+1) == 1):
 			position = "Manager"
+			personManager = "Root"
 		else:
 			position = "Student"
 		#Check if every nth value is modulus one-hundred assign increment value by one
@@ -47,8 +48,9 @@ def main(argv):
 		
 		#read in 2 values from file
 		RandoName = ''.join(random.sample((FileToRead),2))
-		print ("Number:%s,%s,%s%s,%s,%s,%s" % (num+1, RandoName, RandoName, emailAddress, password, position, Org))
-		#
-		
+		print ("Number:%s,%s,%s%s,%s,%s,%s,%s" % (num+1, RandoName, RandoName, emailAddress, password, position, Org, personManager))
+		#Reset Manager to current name if assigned "Root"
+		if (position == ("Manager")):
+			personManager = RandoName
 if __name__ == "__main__":
 	main(sys.argv[1:])
